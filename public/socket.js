5d34cad38f4e613438153c0725f062ba70b57ee0
sunay kundalwal <sunaykundalwal@outlook.in>
2026-06-20 12:51:19 +0530

    deployment test 9

--- public/socket.js
+++ public/socket.js
@@ -9,7 +9,7 @@ const userName = "AirIt" + Math.floor(Math.random() * 100000);
 //const socketURL =  "https://cozy-tightrope-protegee.ngrok-free.dev"
 //const socketURL =  "https://www.airit.site"
 //const socketURL =  "http://localhost:8080"
-const socketURL = "https://airit-production.up.railway.app/" 
+const socketURL = "https://airit-production-716d.up.railway.app/" 
 
 const socket = io(socketURL, {
     auth: {
